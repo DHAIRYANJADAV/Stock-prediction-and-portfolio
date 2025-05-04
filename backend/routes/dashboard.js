c893f73af3b0ba49786dcf487ea95d977b2f5926
DHAIRYANJADAV <98682683+DHAIRYANJADAV@users.noreply.github.com>
2025-05-04 18:43:10 +0530

    Update dashboard.js

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -5,17 +5,10 @@ const fetch = require("node-fetch");
 
 const STOCK_API_KEY = process.env.STOCK_API_KEY; 
 
-// Dashboard Route
 router.get("/",authenticateJWT, async (req, res) => {
     try {
   
         const [gainers, losers, active, largeCap, midCap, smallCap] = await Promise.all([
-            fetch(`https://financialmodelingprep.com/api/v3/gainers?apikey=${STOCK_API_KEY}`).then(res => res.json()),
-            fetch(`https://financialmodelingprep.com/api/v3/losers?apikey=${STOCK_API_KEY}`).then(res => res.json()),
-            fetch(`https://financialmodelingprep.com/api/v3/actives?apikey=${STOCK_API_KEY}`).then(res => res.json()),
-            fetch(`https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=10000000000&limit=10&apikey=${STOCK_API_KEY}`).then(res => res.json()),
-            fetch(`https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=2000000000&marketCapLowerThan=10000000000&limit=10&apikey=${STOCK_API_KEY}`).then(res => res.json()),
-            fetch(`https://financialmodelingprep.com/api/v3/stock-screener?marketCapLowerThan=2000000000&limit=10&apikey=${STOCK_API_KEY}`).then(res => res.json())
         ]);
 
         res.json({
